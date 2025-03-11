58073abf06b35cf438fb57615daf29d202548449
Miroslav <miroslav.geidt@gmail.com>
2025-03-11 17:02:32 +0300

    add adaptive

--- src/App.vue
+++ src/App.vue
@@ -2,320 +2,320 @@
 
 <template>
   <div class="main-wrapper">
-  <!-- Первая секция -->
-  <section class="main-section">
-    <!-- ограничивающий контейнер нав меню -->
-    <div class="container main-section__nav-container">
-      <!-- Навигационное меню -->
-      <header class="main-section__header">
-        <a href="#"
-          ><img
-            class="main-section__logo"
-            src="/src/assets/icons/logo.svg"
-            alt=""
-        /></a>
-      </header>
-      <nav class="main-section__nav nav-menu">
-        <ul class="nav-menu__list">
-          <li class="nav-menu__list-item">
-            <a href="#about-me">Обо мне</a>
-          </li>
-          <li class="nav-menu__list-item">
-            <a href="#work">Занятия</a>
-          </li>
-          <li class="nav-menu__list-item">
-            <a href="#">Запись</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-
-    <!-- ограничивающий контейнер основного контента первой секции -->
-    <div class="container main-section__content-container">
-      <!-- левая часть первой секции -->
-      <div class="left-wrapper">
-        <img
-          src="/src/assets/img/guitaristPhoto.png"
-          alt="фото гитариста"
-          class="left-wrapper__img"
-        />
-        <div class="left-wrapper__rectangle-blue"></div>
-        <div class="left-wrapper__rectangle-orange"></div>
-        <div class="left-wrapper__rectangle-grey"></div>
+    <!-- Первая секция -->
+    <section class="main-section">
+      <!-- ограничивающий контейнер нав меню -->
+      <div class="container main-section__nav-container">
+        <!-- Навигационное меню -->
+        <header class="main-section__header">
+          <a href="#"
+            ><img
+              class="main-section__logo"
+              src="/src/assets/icons/logo.svg"
+              alt=""
+          /></a>
+        </header>
+        <nav class="main-section__nav nav-menu">
+          <ul class="nav-menu__list">
+            <li class="nav-menu__list-item">
+              <a href="#about-me">Обо мне</a>
+            </li>
+            <li class="nav-menu__list-item">
+              <a href="#work">Занятия</a>
+            </li>
+            <li class="nav-menu__list-item">
+              <a href="#">Запись</a>
+            </li>
+          </ul>
+        </nav>
       </div>
 
-      <!-- правая часть первой секции -->
-      <div class="right-wrapper">
-        <!-- иконки -->
-        <div class="right-wrapper__icons-box icons">
-          <div class="icons__item">
-            <i class="fa-brands fa-skype"></i>
+      <!-- ограничивающий контейнер основного контента первой секции -->
+      <div class="container main-section__content-container">
+        <!-- левая часть первой секции -->
+        <div class="left-wrapper">
+          <img
+            src="/src/assets/img/guitaristPhoto.png"
+            alt="фото гитариста"
+            class="left-wrapper__img"
+          />
+          <div class="left-wrapper__rectangle-blue"></div>
+          <div class="left-wrapper__rectangle-orange"></div>
+          <div class="left-wrapper__rectangle-grey"></div>
+        </div>
+
+        <!-- правая часть первой секции -->
+        <div class="right-wrapper">
+          <!-- иконки -->
+          <div class="right-wrapper__icons-box icons">
+            <div class="icons__item">
+              <i class="fa-brands fa-skype"></i>
+            </div>
+            <div class="icons__item">
+              <i class="fa-brands fa-vk"></i>
+            </div>
+            <div class="icons__item">
+              <i class="fa-brands fa-telegram"></i>
+            </div>
+            <div class="icons__item">
+              <i class="fa-brands fa-instagram"></i>
+            </div>
           </div>
-          <div class="icons__item">
-            <i class="fa-brands fa-vk"></i>
-          </div>
-          <div class="icons__item">
-            <i class="fa-brands fa-telegram"></i>
-          </div>
-          <div class="icons__item">
-            <i class="fa-brands fa-instagram"></i>
-          </div>
-        </div>
 
-        <!-- заголовок -->
-        <h1 class="right-wrapper__title title">
-          Я Виктор
-          <hr class="title__hr" />
-        </h1>
+          <!-- заголовок -->
+          <h1 class="right-wrapper__title title">
+            Я Виктор
+            <hr class="title__hr" />
+          </h1>
 
-        <!-- текст -->
-        <p class="right-wrapper__text">
-          Репетитор игры на гитаре с семилетним опытом преподавания
-        </p>
-
-        <!-- кнопки -->
-        <div class="right-wrapper__button buttons">
-          <!-- обычная кнопка -->
-          <button class="buttons__main-btn">
-            Забронировать <br />
-            урок
-          </button>
-
-          <!-- иконка с текстом -->
-          <div class="right-wrapper__icon-btn buttons__icon-btn">
-            <img src="/src/assets/icons/chat.svg" alt="chat" />
-            <p class="buttons__text">
-              Есть <br />
-              вопросы?
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <!-- декоративный круг -->
-      <div class="main-section__first-elipse elipse">
-        <div class="elipse__subtract"></div>
-      </div>
-    </div>
-
-    <!-- большой декоративный круг -->
-    <div class="main-section__second-elipse elipse elipse_dig">
-      <div class="elipse__subtract elipse__subtract_big"></div>
-    </div>
-  </section>
-
-  <!-- Вторая секция -->
-  <section id="about-me" class="second-section">
-    <div class="container second-section__container">
-      <!-- Заголовок -->
-      <h2 class="second-section__title title">
-        <hr class="title__hr" />
-        Преподаю
-      </h2>
-
-      <!-- Карточки -->
-      <div class="second-section__cards-box">
-        <div class="second-section__cards-item cards-item">
-          <img
-            class="cards-item__img"
-            src="/src/assets/img/guitar3.png"
-            alt="Электро-гитара"
-          />
-          <p class="cards-item__text">
-            Электро <br />
-            гитара
+          <!-- текст -->
+          <p class="right-wrapper__text">
+            Репетитор игры на гитаре с семилетним опытом преподавания
           </p>
-        </div>
 
-        <div class="second-section__cards-item cards-item cards-item_orange">
-          <img
-            class="cards-item__img"
-            src="/src/assets/img/guitar2.png"
-            alt="Электро-гитара"
-          />
-          <p class="cards-item__text">
-            Акустическая <br />
-            гитара
-          </p>
-        </div>
+          <!-- кнопки -->
+          <div class="right-wrapper__button buttons">
+            <!-- обычная кнопка -->
+            <button class="buttons__main-btn">
+              Забронировать <br />
+              урок
+            </button>
 
-        <div class="second-section__cards-item cards-item cards-item_blue">
-          <img
-            class="cards-item__img"
-            src="/src/assets/img/guitar1.png"
-            alt="Электро-гитара"
-          />
-          <p class="cards-item__text">
-            Укулеле <br />
-            гитара
-          </p>
-        </div>
-      </div>
-
-      <!-- Иконки -->
-      <div class="second-section__icons-box icons-box">
-        <div class="second-section__icons-item icons-item">
-          <img
-            src="/src/assets/icons/cup.svg"
-            alt="кубок"
-            class="icons-item__img"
-          />
-          <p class="icons-item__text">
-            <span>7</span> <br />
-            лет <br />
-            преподавания
-          </p>
-        </div>
-
-        <div class="second-section__icons-item icons-item">
-          <img
-            src="/src/assets/icons/APlus.svg"
-            alt="отлично"
-            class="icons-item__img"
-          />
-          <p class="icons-item__text">
-            <span>276</span> <br />
-            отзывов <br />
-            на 5 звезд
-          </p>
-        </div>
-
-        <div class="second-section__icons-item icons-item">
-          <img
-            src="/src/assets/icons/pen.svg"
-            alt="головной убор"
-            class="icons-item__img"
-          />
-          <p class="icons-item__text">
-            <span>314</span> <br />
-            студентов <br />
-            обученно
-          </p>
-        </div>
-
-        <div class="second-section__icons-item icons-item">
-          <img
-            src="/src/assets/icons/musicKey.svg"
-            alt="нота"
-            class="icons-item__img"
-          />
-          <p class="icons-item__text">
-            <span>2000</span> <br />
-            часов <br />
-            преподавания
-          </p>
-        </div>
-      </div>
-
-      <!-- Круги -->
-      <div class="second-section__first-elipse elipse">
-        <div class="elipse__subtract"></div>
-      </div>
-
-      <div class="second-section__second-elipse elipse">
-        <div class="elipse__subtract"></div>
-      </div>
-    </div>
-  </section>
-
-  <section id="work" class="third-section">
-    <div class="container third-section__container">
-      <!-- Левая обертка -->
-      <div class="third-section__left-wrapper">
-        <!-- Заголовок -->
-        <h3 class="third-section__left-title title">
-          <hr class="title__hr" />
-          Обучение
-        </h3>
-      </div>
-
-      <!-- Правая обертка -->
-      <div class="third-section__right-wrapper">
-        <!-- Заголовок -->
-        <h4 class="third-section__right-title title">
-          <hr class="title__hr title__hr_top" />
-          Готовлю <br />
-          виртуозов
-        </h4>
-        <!-- текст -->
-        <p class="third-section__text">
-          Равным образом рамки и место обучения кадров влечет за собой процесс
-          внедрения и модернизации позиций, занимаемых участниками в отношении
-          поставленных задач. Товарищи! реализация намеченных плановых заданий
-          позволяет выполнять важные задания по разработке систем массового
-          участия.
-          <br />
-          <br />
-          Повседневная практика показывает, что рамки и место обучения кадров
-          позволяет оценить значение новых предложений. Не следует, однако
-          забывать, что рамки и место обучения кадров влечет за собой процесс
-          внедрения и модернизации систем массового участия.
-        </p>
-        <div class="right-wrapper__button buttons">
-          <!-- обычная кнопка -->
-          <button class="buttons__main-btn">
-            Забронировать <br />
-            урок
-          </button>
-
-          <!-- иконка с текстом -->
-          <div class="right-wrapper__icon-btn buttons__icon-btn">
-            <img src="/src/assets/icons/chat.svg" alt="chat" />
-            <p class="buttons__text">
-              Есть <br />
-              вопросы?
-            </p>
+            <!-- иконка с текстом -->
+            <div class="right-wrapper__icon-btn buttons__icon-btn">
+              <img src="/src/assets/icons/chat.svg" alt="chat" />
+              <p class="buttons__text">
+                Есть <br />
+                вопросы?
+              </p>
+            </div>
           </div>
         </div>
+
+        <!-- декоративный круг -->
+        <div class="main-section__first-elipse elipse">
+          <div class="elipse__subtract"></div>
+        </div>
       </div>
 
-      <!-- Круги внутри контейнера -->
-      <div class="third-section__first-elipse elipse elipse_dig">
+      <!-- большой декоративный круг -->
+      <div class="main-section__second-elipse elipse elipse_dig">
         <div class="elipse__subtract elipse__subtract_big"></div>
       </div>
-    </div>
+    </section>
 
-    <!-- Круг снаружи контейнера -->
-    <div class="third-section__second-elipse elipse elipse_dig">
-      <div class="elipse__subtract elipse__subtract_big"></div>
-    </div>
-  </section>
+    <!-- Вторая секция -->
+    <section id="about-me" class="second-section">
+      <div class="container second-section__container">
+        <!-- Заголовок -->
+        <h2 class="second-section__title title">
+          <hr class="title__hr" />
+          Преподаю
+        </h2>
 
-  <!-- Футер -->
-  <footer class="footer">
-    <div class="container footer__container">
-      <!-- Изображение грифа -->
-      <img
-        src="/src/assets/img/guitarGrif.png"
-        alt="гриф"
-        class="footer__img"
-      />
+        <!-- Карточки -->
+        <div class="second-section__cards-box">
+          <div class="second-section__cards-item cards-item">
+            <img
+              class="cards-item__img"
+              src="/src/assets/img/guitar3.png"
+              alt="Электро-гитара"
+            />
+            <p class="cards-item__text">
+              Электро <br />
+              гитара
+            </p>
+          </div>
 
-      <!-- Обертка для заголовка и иконок -->
-      <div class="footer__wrapper">
-        <h5 class="footer__title title">
-          <hr class="title__hr title__hr_top" />
-          Запишись на <br />
-          пробный урок
-        </h5>
+          <div class="second-section__cards-item cards-item cards-item_orange">
+            <img
+              class="cards-item__img"
+              src="/src/assets/img/guitar2.png"
+              alt="Электро-гитара"
+            />
+            <p class="cards-item__text">
+              Акустическая <br />
+              гитара
+            </p>
+          </div>
+
+          <div class="second-section__cards-item cards-item cards-item_blue">
+            <img
+              class="cards-item__img"
+              src="/src/assets/img/guitar1.png"
+              alt="Электро-гитара"
+            />
+            <p class="cards-item__text">
+              Укулеле <br />
+              гитара
+            </p>
+          </div>
+        </div>
 
         <!-- Иконки -->
-        <div class="footer__icons-box icons">
-          <div class="icons__item">
-            <i class="fa-brands fa-skype"></i>
+        <div class="second-section__icons-box icons-box">
+          <div class="second-section__icons-item icons-item">
+            <img
+              src="/src/assets/icons/cup.svg"
+              alt="кубок"
+              class="icons-item__img"
+            />
+            <p class="icons-item__text">
+              <span>7</span> <br />
+              лет <br />
+              преподавания
+            </p>
           </div>
-          <div class="icons__item">
-            <i class="fa-brands fa-vk"></i>
+
+          <div class="second-section__icons-item icons-item">
+            <img
+              src="/src/assets/icons/APlus.svg"
+              alt="отлично"
+              class="icons-item__img"
+            />
+            <p class="icons-item__text">
+              <span>276</span> <br />
+              отзывов <br />
+              на 5 звезд
+            </p>
           </div>
-          <div class="icons__item">
-            <i class="fa-brands fa-telegram"></i>
+
+          <div class="second-section__icons-item icons-item">
+            <img
+              src="/src/assets/icons/pen.svg"
+              alt="головной убор"
+              class="icons-item__img"
+            />
+            <p class="icons-item__text">
+              <span>314</span> <br />
+              студентов <br />
+              обученно
+            </p>
           </div>
-          <div class="icons__item">
-            <i class="fa-brands fa-instagram"></i>
+
+          <div class="second-section__icons-item icons-item">
+            <img
+              src="/src/assets/icons/musicKey.svg"
+              alt="нота"
+              class="icons-item__img"
+            />
+            <p class="icons-item__text">
+              <span>2000</span> <br />
+              часов <br />
+              преподавания
+            </p>
+          </div>
+        </div>
+
+        <!-- Круги -->
+        <div class="second-section__first-elipse elipse">
+          <div class="elipse__subtract"></div>
+        </div>
+
+        <div class="second-section__second-elipse elipse">
+          <div class="elipse__subtract"></div>
+        </div>
+      </div>
+    </section>
+
+    <section id="work" class="third-section">
+      <div class="container third-section__container">
+        <!-- Левая обертка -->
+        <div class="third-section__left-wrapper">
+          <!-- Заголовок -->
+          <h3 class="third-section__left-title title">
+            <hr class="title__hr" />
+            Обучение
+          </h3>
+        </div>
+
+        <!-- Правая обертка -->
+        <div class="third-section__right-wrapper">
+          <!-- Заголовок -->
+          <h4 class="third-section__right-title title">
+            <hr class="title__hr title__hr_top" />
+            Готовлю <br />
+            виртуозов
+          </h4>
+          <!-- текст -->
+          <p class="third-section__text">
+            Равным образом рамки и место обучения кадров влечет за собой процесс
+            внедрения и модернизации позиций, занимаемых участниками в отношении
+            поставленных задач. Товарищи! реализация намеченных плановых заданий
+            позволяет выполнять важные задания по разработке систем массового
+            участия.
+            <br />
+            <br />
+            Повседневная практика показывает, что рамки и место обучения кадров
+            позволяет оценить значение новых предложений. Не следует, однако
+            забывать, что рамки и место обучения кадров влечет за собой процесс
+            внедрения и модернизации систем массового участия.
+          </p>
+          <div class="right-wrapper__button buttons">
+            <!-- обычная кнопка -->
+            <button class="buttons__main-btn">
+              Забронировать <br />
+              урок
+            </button>
+
+            <!-- иконка с текстом -->
+            <div class="right-wrapper__icon-btn buttons__icon-btn">
+              <img src="/src/assets/icons/chat.svg" alt="chat" />
+              <p class="buttons__text">
+                Есть <br />
+                вопросы?
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <!-- Круги внутри контейнера -->
+        <div class="third-section__first-elipse elipse elipse_dig">
+          <div class="elipse__subtract elipse__subtract_big"></div>
+        </div>
+      </div>
+
+      <!-- Круг снаружи контейнера -->
+      <div class="third-section__second-elipse elipse elipse_dig">
+        <div class="elipse__subtract elipse__subtract_big"></div>
+      </div>
+    </section>
+
+    <!-- Футер -->
+    <footer class="footer">
+      <div class="container footer__container">
+        <!-- Изображение грифа -->
+        <img
+          src="/src/assets/img/guitarGrif.png"
+          alt="гриф"
+          class="footer__img"
+        />
+
+        <!-- Обертка для заголовка и иконок -->
+        <div class="footer__wrapper">
+          <h5 class="footer__title title">
+            <hr class="title__hr title__hr_top" />
+            Запишись на <br />
+            пробный урок
+          </h5>
+
+          <!-- Иконки -->
+          <div class="footer__icons-box icons">
+            <div class="icons__item">
+              <i class="fa-brands fa-skype"></i>
+            </div>
+            <div class="icons__item">
+              <i class="fa-brands fa-vk"></i>
+            </div>
+            <div class="icons__item">
+              <i class="fa-brands fa-telegram"></i>
+            </div>
+            <div class="icons__item">
+              <i class="fa-brands fa-instagram"></i>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </footer>
+    </footer>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import './assets/sass/normalize.scss'
 import './assets/sass/reset.scss'
 import './assets/sass/style.sass'
+import './assets/sass/desktopStyle.sass'
+import './assets/sass/verticalTablet.sass'
+import './assets/sass/horizontalPhone.sass'
+import './assets/sass/verticalPhone.sass'
 
 import { createApp } from 'vue'
 import App from './App.vue'
